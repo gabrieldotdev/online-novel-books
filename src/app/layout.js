cab952a9c9cbb06d8e8a8b5f0e2910096122b3b4
Hoàng Thái Ninh <hoangthaininh.hgn@gmail.com>
2024-03-03 03:49:06 +0700

    style: edit the file extension and reconfigure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from 'next/font/google';
 
-import '@/styles/globals.css';
-import '@/styles/_base.scss';
+import '@/styles/globals.scss';
 
 const inter = Inter({ subsets: ['latin'] });
 
